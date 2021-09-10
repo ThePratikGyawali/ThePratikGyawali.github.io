942357b72b352fdebe8d02352e34600b874db891
Pratik Gyawali <62550510+ThePratikGyawali@users.noreply.github.com>
2021-09-10 22:29:19 +0545

    f

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# iameXfilPr4tik
+# Pratik Gyawali | eXfilPr4tik
 
+Pratik Gyawali | I am a tech enthusuastic high school science student from Nepal. I'm interested in information security.
---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+<script>
+    $(document).ready(function() {
+      // for text animation
+      var typed = new Typed('#typed', {
+        strings: ['InfoSec Enthusiast','CTF Player','eXfilPr4tik'],
+        showCursor: true,
+        cursorChar: '|',
+        autoInsertCss: true,
+        typeSpeed: 60,
+        backSpeed: 20,
+        backDelay: 3000,
+        loop: true,
+      });
+    });
+  </script>
